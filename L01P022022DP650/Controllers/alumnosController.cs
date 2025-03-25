d7874db658d2ed47fb22fc905eba34d1f1eaa5c8
DamianDev-Heaven <harryadonay62@gmail.com>
2025-03-25 17:36:56 -0600

    Uso del scaffolding

---
+++ L01P022022DP650/Controllers/alumnosController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using L01P022022DP650.Models;
+using L01P022022DP650.Models.Data;
+
+namespace L01P022022DP650.Controllers
+{
+    public class alumnosController : Controller
+    {
+        private readonly NotasContext _context;
+
+        public alumnosController(NotasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: alumnos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Alumnos.ToListAsync());
+        }
+
+        // GET: alumnos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alumnos = await _context.Alumnos
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            return View(alumnos);
+        }
+
+        // GET: alumnos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: alumnos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,codigo,nombre,apellidos,dui,estado")] alumnos alumnos)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(alumnos);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumnos);
+        }
+
+        // GET: alumnos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alumnos = await _context.Alumnos.FindAsync(id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+            return View(alumnos);
+        }
+
+        // POST: alumnos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,codigo,nombre,apellidos,dui,estado")] alumnos alumnos)
+        {
+            if (id != alumnos.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(alumnos);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!alumnosExists(alumnos.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(alumnos);
+        }
+
+        // GET: alumnos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var alumnos = await _context.Alumnos
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (alumnos == null)
+            {
+                return NotFound();
+            }
+
+            return View(alumnos);
+        }
+
+        // POST: alumnos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var alumnos = await _context.Alumnos.FindAsync(id);
+            if (alumnos != null)
+            {
+                _context.Alumnos.Remove(alumnos);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool alumnosExists(int id)
+        {
+            return _context.Alumnos.Any(e => e.id == id);
+        }
+    }
+}
